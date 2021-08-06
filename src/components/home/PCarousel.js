70c0b0ad312898466329f2d81bc4674bcabd32d0
JenChieh <jcs090218@gmail.com>
2021-08-07 03:39:54 +0800

    New set up

--- src/components/home/PCarousel.js
+++ src/components/home/PCarousel.js
@@ -10,50 +10,55 @@ import React from 'react';
 
 import {
   Carousel,
-  Container,
+  Container, Row, Col,
 } from 'react-bootstrap';
 
 import slide1 from '../../images/slides/1.png';
 import slide2 from '../../images/slides/2.png';
 import slide3 from '../../images/slides/3.png';
 
+import title from '../../images/title.png';
+
 class PCarousel extends React.Component {
   state = {};
   render () {
     return (
-      <Container fluid style={{backgroundColor: "#121416"}} className="text-left">
+      <Container fluid style={{backgroundColor: "#121416"}}>
         <Container>
-          <Carousel fade>
-            <Carousel.Item>
-              <img className="d-block w-100"
-                   src={ slide1 }
-                   alt="First slide"/>
-              <Carousel.Caption>
-                <h3>First slide label</h3>
-                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-              </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-              <img className="d-block w-100"
-                   src={ slide2 }
-                   alt="Second slide" />
-
-              <Carousel.Caption>
-                <h3>Second slide label</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-              </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-              <img className="d-block w-100"
-                   src={ slide3 }
-                   alt="Third slide" />
-
-              <Carousel.Caption>
-                <h3>Third slide label</h3>
-                <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-              </Carousel.Caption>
-            </Carousel.Item>
-          </Carousel>
+          <Row className="p-4">
+            <Col xs lg="4" className="justify-content-center align-self-center">
+              <Container fluid className="text-light text-left">
+                <Row>
+                  <Col>
+                    <h1><img src={ title } alt="title"/></h1>
+                    <h4 className="text-secondary">
+                      { this.props.getContext("Carousel.Quote") }
+                    </h4>
+                    { this.props.getContext("Carousel.Text") }
+                  </Col>
+                </Row>
+              </Container>
+            </Col>
+            <Col className="d-none d-lg-block">
+              <Carousel fade>
+                <Carousel.Item>
+                  <img className="d-block w-100"
+                       src={ slide1 }
+                       alt="Slide1"/>
+                </Carousel.Item>
+                <Carousel.Item>
+                  <img className="d-block w-100"
+                       src={ slide2 }
+                       alt="Slide2" />
+                </Carousel.Item>
+                <Carousel.Item>
+                  <img className="d-block w-100"
+                       src={ slide3 }
+                       alt="Slide3" />
+                </Carousel.Item>
+              </Carousel>
+            </Col>
+          </Row>
         </Container>
       </Container>
     );
